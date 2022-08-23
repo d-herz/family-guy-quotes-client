d6b9ba19e85bd033a7042c4456f6dcab436929e6
Daniel Hyres <danhyres@gmail.com>
2022-08-23 17:04:47 -0400

    wip

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
+//add class of 'hidden' to section, which (from CSS) will set display to 'none'
 document.querySelector('section').classList.add('hidden')
 
+//add event listener to the button, when button clicked run the getFetch function
 document.querySelector('button').addEventListener('click', getFetch)
 
 
 
 function getFetch(){
 
-  
+  const url = `https://breaking-bad-database-builder.herokuapp.com/api`
+
+  document.querySelector('#PG').classList.add('hidden')
+  document.querySelector('#SG').classList.add('hidden')
+  document.querySelector('#BG').classList.add('hidden')
+  document.querySelector('#LG').classList.add('hidden')
+  document.querySelector('#CG').classList.add('hidden')
+  document.querySelector('#MG').classList.add('hidden')
+  document.querySelector('#GQ').classList.add('hidden')
+  document.querySelector('#JS').classList.add('hidden')
+  document.querySelector('#CB').classList.add('hidden')
+  document.querySelector('#MW').classList.add('hidden')
 
 
 }
