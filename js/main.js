c1c5c505833d29f587d87830328a224c58a6b2a7
Daniel Hyres <danhyres@gmail.com>
2022-08-23 17:51:38 -0400

    working js

--- js/main.js
+++ js/main.js
@@ -8,8 +8,10 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
 
+  //assign the heroku link to a variable for use later
   const url = `https://breaking-bad-database-builder.herokuapp.com/api`
 
+  //on function call, add class of 'hidden' to all of the image containers (so they disappear)
   document.querySelector('#PG').classList.add('hidden')
   document.querySelector('#SG').classList.add('hidden')
   document.querySelector('#BG').classList.add('hidden')
@@ -21,5 +23,21 @@ function getFetch(){
   document.querySelector('#CB').classList.add('hidden')
   document.querySelector('#MW').classList.add('hidden')
 
+  //This conditional checks if the section is hidden, and unhides it. Mostly for asthetics so we don't have an empty container sitting there before the button is clicked for the first time
+  if( document.querySelector('section').classList.contains('hidden')){
+    document.querySelector('section').classList.toggle('hidden')
+  }
+
+  //this is our fetch function with a parameter of URL declared earlier
+  //the 2nd .then is where we decide what to do with the objects being returned to us, which contain the quotes and author
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data[1].quote)
+    })
+    
+    .catch(err => {
+      console.log(err)
+    })
 
 }
